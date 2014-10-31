314a75a62488fd3a1dc067e9cc241b438432cf85
boubacar2 <boubacar.fane2@gmail.com>
2014-10-31 22:17:06 +0100

    [US3][Tache8] Page de projet

--- DYF-Scrum/app/actions/affiche_projet.php
+++ DYF-Scrum/app/actions/affiche_projet.php
@@ -1,12 +1,10 @@
 <?php
-
-	include"bd-connexion.php"; 
-	
-	$id = $_SESSION['id'];     //c'est ca qui marche pas;   que ca soit id ou pseudo
+include"bd-connexion.php"; 	
+	$id = $_SESSION['id'];     
 	$pseudo = $_SESSION['pseudo'];
-	//$query = "SELECT * FROM  projet";   c'est ce que j'avais avant
-	
 
+
+	
 	 $query="SELECT PROJET.ID, PROJET.NOM, PROJET.DESCRIPTION,PROJET.LIEN_GIT
 	  FROM PROJET JOIN PARTICIPER 
 	  ON PARTICIPER.ID_PROJET = PROJET.ID JOIN UTILISATEUR 
@@ -14,46 +12,6 @@
 	   WHERE UTILISATEUR.ID =".$id; 
 
 	$result =$conn->query($query);
-	if( $result == TRUE){
-		$num=mysqli_num_rows($result);
 
-?>      
-
-
- <div class="page-header"><h3>Liste de(s) projet(s)  sur le(s)quel(s) travail  <small><?php echo $pseudo; ?></small></h3></div>
-		<div class="container">     
-    		<div class="row"> 
-    		<div class="col-xs-12 col-sm-10 col-md-6"> 
-       	    <table class="table table-bordered table-striped table-hover"> 
-               
-          <thead> 
-          <tr> 
-          <td>NOM PROJET</td> 
-          <td>DESCRIPTION PROJET</td> 
-          <td>EMPLACEMENT DU DEPOT</td> 
-          <td>ID</td>
-          </tr> 
-          </thead> 
-		
-		<?php
-		while($row = $result->fetch_assoc()){
-
-			?>
-			 <tbody> 
-          <tr class="success"> 
-          	<td><a href="#"><?php echo $row['NOM']; ?></a></td> 
-          <td><?php echo $row['DESCRIPTION']; ?></td>  
-          <td><?php echo $row['LIEN_GIT']; ?></td> 
-          <td><?php echo $row['ID']; ?></td>
-          </tr>            	
-	     
-        <?php
-     }
-
-		}
-	else{
-		echo "There are any error(s) with this request"; 		
-	}	
 $conn->close();
-	
-?>
+	?>
