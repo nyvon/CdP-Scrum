509fab88246452daa8f15398b59202164e01c8fb
nyvon <nicolasyvon33@gmail.com>
2014-11-03 23:41:17 +0100

    [ALL US]Résolution des conflits du merge
    Résolution des conflits du merge

--- DYF-Scrum/app/actions/backlog.php
+++ DYF-Scrum/app/actions/backlog.php
@@ -1,3 +1,15 @@
-<?
-	include "bd-connexion.php";
+<?php 
+	include"bd-connexion.php"; 	
     $id_projet = $_GET['id_projet'];
+
+
+	
+	 $query="SELECT USER_STORY.* FROM USER_STORY JOIN BACKLOG 
+	  ON BACKLOG.ID_USER_STORY = USER_STORY.ID JOIN PROJET 
+	  ON PROJET.ID = BACKLOG.ID_PROJET
+	   WHERE PROJET.ID =".$id_projet; 
+
+	$result =$conn->query($query);
+
+	$conn->close();
+?>
