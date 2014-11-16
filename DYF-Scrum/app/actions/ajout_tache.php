abfc27ac82707a95475523d56ea323d8819ebdb5
nyvon <nicolasyvon33@gmail.com>
2014-11-16 22:28:35 +0100

    [ALL US]Modification générales
    Cohérence du menu dans chacune des page (ajout des liens qui manquaient
    dans certains pages)

--- DYF-Scrum/app/actions/ajout_tache.php
+++ DYF-Scrum/app/actions/ajout_tache.php
@@ -5,16 +5,24 @@ include "bd-connexion.php";
 	$inti =$_POST["intitule"];
 	$descri = $_POST["desc"];
 	$cout = $_POST["cout"];
-	
+	$id_us = $_POST["us"];
 	
 	if( !empty($_POST['intitule']) AND !empty($_POST['desc']) AND !empty($_POST['cout']))
 	{
-	$query = "INSERT INTO TACHE ( INTITULE, DESCRIPTION, COUT) VALUES ('".$inti."','".$descri."','".$cout."')";
+	$query = "INSERT INTO TACHE ( INTITULE, DESCRIPTION, COUT, ID_ETAT) VALUES ('".$inti."','".$descri."','".$cout."',1)";
 	if( $conn->query($query) == FALSE)
 		echo '<div class="alert alert-danger" role="alert">Error : Echec creation de la tache .</div>';
-	else
-		echo '<legend>Création tache</legend>
-	<div class="alert alert-success" role="alert">Tache créée avec succes</div>';
+	else{
+		$req = $conn->query("SELECT ID FROM TACHE WHERE INTITULE ='".$inti."' AND DESCRIPTION ='".$descri."' AND COUT =".$cout);
+		$row = $req->fetch_assoc();
+		$req2 = $conn->query("INSERT INTO ASSOCIER (ID_USER_STORY, ID_TACHE) VALUES ('".$id_us."','".$row['ID']."')");
+		if($req2 != FALSE)
+			echo '<legend>Création tache</legend><div class="alert alert-success" role="alert">Tache créée avec succes</div>';
+		else
+			echo '<div class="alert alert-danger" role="alert">Error : Echec creation de la tache .</div>';
+
+
+}
 	?>
 		<script type="text/javascript">
 		
---
+++ DYF-Scrum/app/actions/form_tache.php
@@ -0,0 +1,2 @@
+<?php
+	include 'bd-connexion.php';
