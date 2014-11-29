10c1a8950ed83f8e6dcdbd6b4ee52ea72130e4a8
nyvon <nicolasyvon33@gmail.com>
2014-11-29 23:17:36 +0100

    [US 19]Burndown Chart
    Burndown Chart par sprint du projet

--- DYF-Scrum/app/actions/verif_changer_etat_tache.php
+++ DYF-Scrum/app/actions/verif_changer_etat_tache.php
@@ -13,11 +13,21 @@ if ( !empty($_POST['id_etat']) AND !empty($_POST['id_tache']))
 	$query = "UPDATE TACHE SET ID_ETAT=".$id_etat." WHERE ID=".$id_tache.";";
 	if( $conn->query($query) == FALSE)
 		echo '<div class="alert alert-danger" role="alert">Impossible de créer votre compte.</div>';
-	else
-
+	else{
+		$query2 = "SELECT USER_STORY.ID FROM USER_STORY JOIN BACKLOG ON BACKLOG.ID_USER_STORY = USER_STORY.ID WHERE BACKLOG.ID_PROJET =".$id_projet;
+		$result = $conn->query($query2);
+		while($row = $result->fetch_assoc()){
+			$query3 = "SELECT * FROM TACHE JOIN ASSOCIER ON ASSOCIER.ID_TACHE = TACHE.ID WHERE ASSOCIER.ID_USER_STORY =".$row["ID"]." AND TACHE.ID_ETAT != 3 ";
+			$result2 = $conn->query($query3);
+			if($result2->num_rows == 0)
+				$conn->query("UPDATE USER_STORY SET ID_ETAT = 3 WHERE ID=".$row["ID"]);
+			else
+				$conn->query("UPDATE USER_STORY SET ID_ETAT = 1 WHERE ID=".$row["ID"]);
+		}
 
 		echo '<legend>Inscription</legend>
 	<div class="alert alert-success" role="alert">Etat de la tâche bien modifié.</div>';
+}
 	?>
 	<script type="text/javascript">
 	<!--
